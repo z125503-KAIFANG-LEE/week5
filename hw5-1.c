2d879549ac17fe804d6edb132604c5df27afaffa
z125503-KAIFANG-LEE <z125503@shibaura-it.ac.jp>
2025-11-07 20:31:25 +0900

    hw5-1.c

--- hw5-1.c
+++ hw5-1.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i, max, n ;
+        i = 1;
+                printf("max? ");
+        scanf("%d", &max);
+	while( i<=max ) {
+		printf("%d\n", i);
+		i = i * 2;
+	}
+	return 0;
+}
+
 
